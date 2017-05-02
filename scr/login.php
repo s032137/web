7040cd498a1b1366810e063f2e93da3a5dcef384
TAM Tin Lun <yinktam0625@icloud.com>
2017-05-03 01:34:50 +0800

    Login.php

--- scr/login.php
+++ scr/login.php
@@ -1,27 +1,62 @@
 <?php
-session_start();
+require_once('layout.php');
+head('Login');
 if (!isset($_SESSION['email'])) {
-
+    
 ?>
-<html>
-    <head>
-        <meta charset="utf-8"/>
-        <title>Login</title>
-        
-         <!-- bootstrap css-->
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery.bootstrapvalidator/0.5.0/css/bootstrapValidator.min.css">
-        <!--script src-->
-        <script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-        <script src = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-        <script src = "https:////cdnjs.cloudflare.com/ajax/libs/bootstrap-validator/0.4.5/js/bootstrapvalidator.min.js"></script>
-        <style>
-            #success_message {
-                display: none;
+
+        <script type="text/javascript">
+            function checkSubmit() {
+                if (checkEmail() && checkPwd()) {
+                    return true;
+                } else {
+                    alert('Please finish the form');
+                    return false;
+                }
+                
             }
-        </style>
+            
+            function checkEmail() {
+                var x = document.getElementById("inputEmail").value;
+                var y = document.getElementById("emailDiv");
+                var z = document.getElementById("emailSymbol");
+                
+                 var atpos = x.indexOf("@");
+                 var dotpos = x.lastIndexOf(".");
+                
+                if (atpos < 1 || dotpos < atpos + 2 || dotpos +2 >= x.length) {
+                    y.className="form-group has-error has-feedback";
+                    z.className="glyphicon glyphicon-remove form-control-feedback";
+                    z.style.visibility = "visible";
+                    return false;
+                } else {
+                    y.className="form-group has-success has-feedback";
+                    z.className="glyphicon glyphicon-ok form-control-feedback";
+                    z.style.visibility = "visible";
+                    return true;
+                }
+            }
+            
+            function checkPwd() {
+                var x = document.getElementById("inputPassword").value;
+                var y = document.getElementById("pwdDiv");
+                var z = document.getElementById("pwdSymbol");
+                if (x.length <= 7) {
+                    y.className="form-group has-error has-feedback";
+                    z.className="glyphicon glyphicon-remove form-control-feedback";
+                    z.style.visibility = "visible";
+                    return false;
+                } else {
+                    y.className="form-group has-success has-feedback";
+                    z.className="glyphicon glyphicon-ok form-control-feedback";
+                    z.style.visibility = "visible";
+                    return true;
+                }
+            }
+        </script>
     </head>
+    
+    
     
     
     
@@ -29,17 +64,19 @@ if (!isset($_SESSION['email'])) {
     <!--Login Form-->
 <div class="container">
     <h1>Login Form</h1>
-    <form id="loginform" class="form-horizontal" action="loginprocess.php" method="post">
-        <div class="form-group">
+    <form id="loginform" class="form-horizontal" action="loginprocess.php" onsubmit="return checkSubmit()" method="post">
+        <div class="form-group" id="emailDiv">
             <label for="inputEmail" class="control-label col-xs-3">Email</label>
             <div class="col-xs-9">
-                <input name="email" type="email" class="form-control" id="inputEmail" placeholder="Email">
+                <input name="email" type="text" class="form-control" id="inputEmail" placeholder="Email" oninput="checkEmail()">
+                <span id="emailSymbol" style="visibility: hidden"></span>
             </div>
         </div>
-        <div class="form-group">
+        <div class="form-group" id="pwdDiv">
             <label for="inputPassword" class="control-label col-xs-3">Password</label>
             <div class="col-xs-9">
-                <input name="password" type="password" class="form-control" id="inputPassword" placeholder="Password">
+                <input name="password" type="password" class="form-control" id="inputPassword" placeholder="Password" oninput="checkPwd()">
+                <span id="pwdSymbol" style="visibility: hidden"></span>
             </div>
         </div>
         <div class="form-group">
@@ -48,6 +85,7 @@ if (!isset($_SESSION['email'])) {
             </div>
         </div>
     </form>
+</div>
     
 </body>
 </html>
@@ -59,30 +97,3 @@ if (!isset($_SESSION['email'])) {
     
 }
 ?>
-<script>
-    $(document).ready(function() {
-        $('#loginform').bootstrapValidator({
-           
-            feedbackIcons: {
-                valid: 'glyphicon glyphicon-ok',
-                invalid: 'glyphicon glyphicon-remove',
-                validating: 'glyphicon glyphicon-refresh'
-            },
-            fields: {
-                email: {
-                    validators: {
-                        notEmpty: {
-                            message: 'Please supply your email address'
-                        },
-                        emailAddress: {
-                            message: 'Please supply a valid email address'
-                        }
-                    }
-                }
-                }
-            })
-
-});
-
-
-</script>
